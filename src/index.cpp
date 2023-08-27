479bf1876120f04827fab3fb21ad0e29c115e4fb
Gelado047 <juniorthesouza017@gmail.com>
2023-08-27 17:20:52 -0300

    StepTwo

--- src/index.cpp
+++ src/index.cpp
@@ -1,7 +1,17 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
 using namespace std;
-
 int main(){
-    string s ="hello";
-    cout <<s[0] <<endl;
+    int i,j,k;
+    string s,t;
+    int key;
+    cout<<"Inserir chave\n";
+    cin>>key;
+    cout<<"Insira a mensagem\n";
+    cin>>s;
+    for(i=0;i<s.size();i++){
+        t+=(s[i]-'A'+key)%26+'A';
+    }
+    cout<<"\n\nA mensagem criptografada é "<<t<<'\n';
+    return 0;
 }
